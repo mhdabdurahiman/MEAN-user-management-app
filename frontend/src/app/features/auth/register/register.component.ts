d6138e6e41e54deb084fb0be2529bb92b96e0340
mhdabdurahiman <abdurahmanettuveettil@gmail.com>
2024-08-01 16:11:07 +0530

    feat(frontend/auth): Add validation for form fields

--- frontend/src/app/features/auth/register/register.component.ts
+++ frontend/src/app/features/auth/register/register.component.ts
@@ -1,12 +1,43 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, NgIf, NgClass],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      lname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      mobile: ['', Validators.required],
+      password: ['', Validators.required],
+      cpassword: ['', Validators.required]
+    },{
+      validator: this.passwordMatchValidator
+    });
+  }
+
+  passwordMatchValidator(group: FormGroup) {
+    const passwordControl = group.get('password');
+    const confirmPasswordControl = group.get('cpassword');
+
+    if (!passwordControl || !confirmPasswordControl) return null;
+
+    return passwordControl.value === confirmPasswordControl.value ? null : { mismatch: true };
+  }
+
+  onSubmit(){
+    if (this.registerForm.valid){
+      console.log('Form Submitted:', this.registerForm.value);
+
+    }
+  }
 }
