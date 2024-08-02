55e0b7f6bd3dd547364fd08ecea50f58ce06b0f6
mhdabdurahiman <abdurahmanettuveettil@gmail.com>
2024-08-02 19:37:23 +0530

    refactor(frontend): Changed the folder structure into more simple

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './features/auth/login/login.component';
-import { RegisterComponent } from './features/auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
